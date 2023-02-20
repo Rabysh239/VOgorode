bf123b6b7e468e94ecada0a8fed90797585852ad
Rabysh Andrian <284603@niuitmo.ru>
2023-02-20 22:43:43 +0300

    continue rebasing

--- LandscapeService/src/test/java/ru/tinkoff/academy/landscape/IntegrationEndpointTest.java
+++ LandscapeService/src/test/java/ru/tinkoff/academy/landscape/IntegrationEndpointTest.java
@@ -1,4 +1,4 @@
-package ru.tinkoff.academy.landscape.controller;
+package ru.tinkoff.academy.landscape;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +13,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class SystemControllerTest {
+class IntegrationEndpointTest {
     @Autowired
     private MockMvc mockMvc;
 
