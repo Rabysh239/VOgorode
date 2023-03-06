a716c273571796823b41961e2757013e68761554
Rabysh Andrian <284603@niuitmo.ru>
2023-03-06 23:33:52 +0300

    hw2 fix service api interaction

--- LandscapeService/src/main/java/ru/tinkoff/academy/landscape/controller/SystemController.java
+++ LandscapeService/src/main/java/ru/tinkoff/academy/landscape/controller/SystemController.java
@@ -12,7 +12,6 @@ import java.util.Map.Entry;
 import static org.springframework.http.HttpStatus.SERVICE_UNAVAILABLE;
 import static org.springframework.http.ResponseEntity.ok;
 import static org.springframework.http.ResponseEntity.status;
-import static ru.tinkoff.academy.landscape.data.ReadinessStatus.NOK;
 
 @RestController
 @RequiredArgsConstructor
@@ -34,7 +33,7 @@ public class SystemController {
     @GetMapping("/readiness")
     public ResponseEntity<Entry<String, String>> getReadiness() {
         Entry<String, String> readiness = service.getReadiness();
-        if (NOK.toString().equals(readiness.getValue())) {
+        if ("NOK".equals(readiness.getValue())) {
             return status(SERVICE_UNAVAILABLE).build();
         }
         return ok(readiness);
