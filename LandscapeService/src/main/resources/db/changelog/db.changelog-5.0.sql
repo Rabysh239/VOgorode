d57f8742a57a4619621a9a2c50bf8089fe126100
Andrian Rabysh <284603@niuitmo.ru>
2023-03-23 19:44:20 +0300

    hw5 (#6)
    * hw5
    
    * hw5 fix postgres volume data
    
    * hw5 add volumes
    
    * hw5 fix strange user_type table

---
+++ LandscapeService/src/main/resources/db/changelog/db.changelog-5.0.sql
@@ -0,0 +1,15 @@
+--liquibase formatted sql
+
+--changeset Rabysh239:1
+create extension if not exists "uuid-ossp";
+
+create table if not exists users
+(
+    id        UUID default uuid_generate_v4() primary key,
+    user_type varchar(255) not null,
+    login     varchar(255) not null,
+    email     varchar(255) not null,
+    phone     varchar(255) not null,
+    created   timestamp    not null,
+    updated   timestamp    not null
+);
---
+++ LandscapeService/src/main/resources/db/changelog/db.changelog-5.2.sql
@@ -0,0 +1,29 @@
+--liquibase formatted sql
+
+--changeset Rabysh239:3 splitStatements:false
+create or replace procedure update_user_type(p_user_type_id smallint, p_batch_size integer, p_pause_time integer)
+    language plpgsql
+as
+$$
+declare
+    v_size           integer;
+    v_processed_size integer := 0;
+begin
+    select count(*) into v_size from users;
+    while v_processed_size < v_size
+        loop
+            raise notice '% - Start processing next % rows.', now(), p_batch_size;
+            update users
+            set user_type_id = p_user_type_id
+            where id in (
+                select id
+                from users
+                order by created
+                limit p_batch_size offset v_processed_size
+            );
+            v_processed_size := v_processed_size + p_batch_size;
+            perform pg_sleep(p_pause_time);
+            raise notice '% - Committed % rows. Migration paused for % sec.', now(), p_batch_size, p_pause_time;
+        end loop;
+end;
+$$;
